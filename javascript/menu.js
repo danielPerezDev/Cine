b6e047b9886ca764d5ed284c8a4759caf0dafb2c
danielPerezDev <danicamusalbert31@gmail.com>
2024-06-08 19:24:29 -0300

    funciones javascript

---
+++ javascript/menu.js
@@ -0,0 +1,9 @@
+//MENU
+function desplegar(){
+    var $lista1 = document.getElementById("menu2");
+    $lista1.style="visibility: none";
+}
+function Nodesplegar(){
+    var $lista1 = document.getElementById("menu2");
+    $lista1.style="display: none";
+}
